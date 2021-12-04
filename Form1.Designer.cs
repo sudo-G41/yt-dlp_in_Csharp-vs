bf7226a2fb19bd8d52d783489c3d7da28dd70585
sudo-G41 <usa7372@gmail.com>
2021-12-05 02:56:40 +0900

    버튼 만들기

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows.Forms;
+
+namespace youtube
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);//창 사이즈 조절
+            this.Text = "Form1";
+            this.FormBorderStyle=FormBorderStyle.FixedSingle;//창 사이즈 고정
+
+            btn1 = new Button();
+            btn1.Name = "btn1";
+            btn1.Text = "???";
+            btn1.Location = new System.Drawing.Point(100, 20);
+            btn1.Size = new System.Drawing.Size(200,30);
+            
+            btn2 = new Button();
+            btn2.Name = "btn2";
+            btn2.Text = "File";
+            btn2.Location = new System.Drawing.Point(100, 70);
+            btn1.Size = new System.Drawing.Size(200,30);
+
+            this.Controls.Add(btn1);
+            this.Controls.Add(btn2);
+
+            this.MouseDown += new MouseEventHandler(MousePointXY);
+        }
+
+        private Button btn1;
+        private Button btn2;
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace youtube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            btn1.Click += btn1Click;
+            btn2.Click += btn2Click;
+        }
+
+        private void btn1Click(object sender, EventArgs e){
+            if(btn1.Text.Length<3){
+                btn1.Text+="?";
+            }
+            else{
+                btn1.Text = "";
+            }
+            Console.WriteLine(btn1.Text);
+        }
+
+        private void btn2Click(object sender, EventArgs e){//폴더 선택 이벤트
+            CommonOpenFileDialog d = new CommonOpenFileDialog();
+            d.IsFolderPicker = true;
+            if(d.ShowDialog()==CommonFileDialogResult.Ok){
+                btn1.Text = d.FileName;
+                Console.WriteLine(btn1.Text);
+                String option = "-o "+d.FileName+"\"\\download\\%(title)s.%(ext)s\" https://www.youtube.com/watch?v=1vryJp_ylVQ -v";
+                var sample = Process.Start(Application.StartupPath+"/yt-dlp.exe",option);
+                Console.WriteLine(option);
+            }
+        }
+
+        private void MousePointXY(object sender, MouseEventArgs e){
+            Console.WriteLine("Sender : {0}", ((Form)sender).Text);
+            Console.WriteLine("X : {0}, Y : {1}", e.X, e.Y);
+            Console.WriteLine("Button : {0}, Clicks : {1}", e.Button, e.Clicks);
+            Console.WriteLine();
+        }
+    }
+}
